89570e14e9e06965429849a6e807ec4ed40662fc
Anithpavan <168651164+Anithpavan@users.noreply.github.com>
2026-01-28 23:20:23 +0530

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const { port } = require('./config/env');
-const connectDB = require('./config/db');
-
-connectDB();
-
-app.listen(port, () => {
-  console.log(` Server running on port ${port}`);
-});
